3f4a1bf82a6c4ef55802567e4940cb63bbfc49d4
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-01 23:28:02 +0500

    Update 04_typing_speed.py

--- 04_typing_speed.py
+++ 04_typing_speed.py
@@ -1,7 +1,7 @@
 import time # import time module for time calculation
 import re   # import re for regular expression
 sample_stirng = "Hi this is asim how are you i think you asldfjls lsjflks dlskjfd slkj slfdkj sa;lkf "
-print(len(re.findall(r'\w+', sample_stirng)))
+print(len(re.findall(r'\w+', sample_stirng)))    # prints random string on output screen
 start_time = time.time()
 new_str = input()
 end_time = time.time()
