9421726b924a9bb9d93cece235dff32ffeaf6b70
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-02 23:05:56 +0500

    Update 01_track_phone_number.py

--- 01_track_phone_number.py
+++ 01_track_phone_number.py
@@ -1,7 +1,7 @@
 import phonenumbers
 from phonenumbers import timezone, carrier, geocoder
 
-target_number = input("enter phone number with country code")
+target_number   = input("enter phone number with country code")
 number        = phonenumbers.parse(target_number)
 target_timezone = timezone.time_zones_for_number(number)
 target_carrier  = carrier.name_for_number(number,'en')
